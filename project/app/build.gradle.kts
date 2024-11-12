c7f84278a1ee2bc2b142fb47df6bbf76854b271b
Maciej Waleryn <xvexisu@gmail.com>
2024-11-12 23:22:00 +0100

    chore: update maven dependencies

--- project/app/build.gradle.kts
+++ project/app/build.gradle.kts
@@ -3,17 +3,17 @@ plugins { application }
 repositories { mavenCentral() }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    implementation("com.google.guava:guava:32.1.1-jre")
+    implementation("com.google.guava:guava:32.3.1-jre")
 
-    compileOnly("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
 
-    testCompileOnly("org.projectlombok:lombok:1.18.30")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
+    testCompileOnly("org.projectlombok:lombok:1.18.34")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
 }
 
 java { toolchain { languageVersion.set(JavaLanguageVersion.of(17)) } }
