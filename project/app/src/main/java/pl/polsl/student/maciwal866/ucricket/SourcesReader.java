12a6e7d46dc9b942d8e183b4e43bacae4cfbcfe3
vexisu <xvexisu@gmail.com>
2025-01-09 16:58:42 +0100

    fix: missing scanner close

--- project/app/src/main/java/pl/polsl/student/maciwal866/ucricket/SourcesReader.java
+++ project/app/src/main/java/pl/polsl/student/maciwal866/ucricket/SourcesReader.java
@@ -15,23 +15,25 @@ import java.util.regex.Pattern;
 
 public class SourcesReader {
     private LinkedHashSet<String> sourcePaths = new LinkedHashSet<>();
-    private static final Pattern IMPORT_PATTERN = Pattern.compile("import ([a-zA-Z][a-zA-Z0-9]*(:[a-zA-Z][a-zA-Z0-9]*)*);");
+    private static final Pattern IMPORT_PATTERN = Pattern
+            .compile("import ([a-zA-Z][a-zA-Z0-9]*(:[a-zA-Z][a-zA-Z0-9]*)*);");
 
     public SourcesReader(String path) {
         sourcePaths.add(path);
         findDependentSources(toFilePath(path));
     }
-   
-    private void findDependentSources(String path){
+
+    private void findDependentSources(String path) {
         try (var reader = new FileReader(path)) {
-            var scanner = new Scanner(reader);
-            while (scanner.hasNext()) {
-                var line = scanner.nextLine();
-                var matcher = IMPORT_PATTERN.matcher(line);
-                if (matcher.find() && sourcePaths.add(matcher.group(1))) {
-                    var importPath = matcher.group(1);
-                    System.out.format("Found new dependent source: %s\n", importPath);
-                    findDependentSources(toFilePath(importPath));
+            try (var scanner = new Scanner(reader)) {
+                while (scanner.hasNext()) {
+                    var line = scanner.nextLine();
+                    var matcher = IMPORT_PATTERN.matcher(line);
+                    if (matcher.find() && sourcePaths.add(matcher.group(1))) {
+                        var importPath = matcher.group(1);
+                        System.out.format("Found new dependent source: %s\n", importPath);
+                        findDependentSources(toFilePath(importPath));
+                    }
                 }
             }
         } catch (IOException e) {
