5d9fc0e1d95d40bc6595b5094b2f567c0783a301
Maciej Waleryn <xvexisu@gmail.com>
2025-01-28 15:50:59 +0100

    feat: add Byte type, change Integer to 16 bit

--- project/app/src/main/java/pl/polsl/student/maciwal866/ucricket/ast/ValueType.java
+++ project/app/src/main/java/pl/polsl/student/maciwal866/ucricket/ast/ValueType.java
@@ -7,17 +7,19 @@ public enum ValueType {
     INTEGER,
     FLOAT,
     BOOLEAN,
+    BYTE,
     NONE;
 
-    public final static ValueType[] NUMERIC_TYPES = { INTEGER, FLOAT };
+    public final static ValueType[] NUMERIC_TYPES = { INTEGER, FLOAT, BYTE };
     public final static ValueType[] LOGIC_TYPES = { BOOLEAN };
-    public final static ValueType[] ALL_TYPES = { INTEGER, FLOAT, BOOLEAN };
+    public final static ValueType[] ALL_TYPES = { INTEGER, FLOAT, BOOLEAN, BYTE };
 
     public LLVMTypeRef getLlvmType() {
         return switch (this) {
             case BOOLEAN -> LLVMInt1Type();
             case FLOAT -> LLVMFloatType();
-            case INTEGER -> LLVMInt8Type();
+            case BYTE -> LLVMInt8Type();
+            case INTEGER -> LLVMInt16Type();
             case NONE -> LLVMVoidType();
             default -> LLVMVoidType();
         };
@@ -27,7 +29,8 @@ public enum ValueType {
         return switch (this) {
             case BOOLEAN -> LLVMInt1TypeInContext(context);
             case FLOAT -> LLVMFloatTypeInContext(context);
-            case INTEGER -> LLVMInt8TypeInContext(context);
+            case BYTE -> LLVMInt16TypeInContext(context);
+            case INTEGER -> LLVMInt16TypeInContext(context);
             case NONE -> LLVMVoidTypeInContext(context);
             default -> LLVMVoidTypeInContext(context);
         };
